4613e5ed2566e75f5d29671f9669fe6a78688446
Tom Brightbill & Jeremy Sabat <pair+tombrightbill.velveetachef@devbootcamp.com>
2014-10-10 19:02:05 -0700

    Second iteration

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -6,8 +6,8 @@ class Sudoku
     @box_array = []
     # @box_side_length = 3
 
-    @current_column = 0
-    @current_row = 0
+    @current_column_index = 0
+    @current_row_index = 0
     @board_array = board_string.split('').each_slice(9).to_a
     @board_array.each{ |row| row.map!{ |element| element == '-' ? element : element.to_i }}
     @box_side_length = Math.sqrt(@board_array[0].length).to_i
@@ -17,34 +17,26 @@ class Sudoku
   def solve
     while empty_cells?
 
-      @board_array.each_with_index do |row, index|
-        if row.include?('-')
-          @current_row = index
-          row.each do |element, index|
-            @current_column = index
+      @board_array.each_with_index do |row, row_index|
+          @current_row_index = row_index
+          row.each_with_index do |element, col_index|
+            @current_column_index = col_index
             if element == '-'
-              element = missing_value
-            else
-              next
+              @board_array[row_index][col_index] = missing_value
             end
           end
-        else
-          next
-        end
       end
     end
   end
 
   def missing_value
-    # row_values = get_row_values
-    # col_values  = get_col_values
-    # box = get_box_values
-    combined_row_col_box = get_box_values + get_col_values + get_row_values
+    holder = get_box_values
+    combined_row_col_box = holder + get_col_values + get_row_values
     combined_row_col_box.delete('-')
     unique = combined_row_col_box.uniq!
 
     if unique.length == 8
-      return POSSIBLE_VALUES - unique
+      return (POSSIBLE_VALUES.inject(:+) - unique.inject(:+))
     else
       return false
     end
@@ -56,19 +48,17 @@ class Sudoku
 
    # return array with values of current row
   def get_row_values
-    @board_array[@current_row]
+    @board_array[@current_row_index]
   end
 
   # return array with values of current col
   def get_col_values
-    @board_array.transpose[@current_row]
+    @board_array.transpose[@current_column_index]
   end
 
   def get_box_row_coordinate
-    # @box_row = @current_row    / @box_side_length
-    # @box_col = @current_column / @box_side_length
-    @box_row = 0
-    @box_col = 0
+    @box_row = @current_row_index    / @box_side_length
+    @box_col = @current_column_index / @box_side_length
   end
 
   def get_box_values
@@ -91,7 +81,7 @@ class Sudoku
 
   # Returns a nicely formatted string representing the current state of the board
   def to_s
-    @board_view = @board_array.each { |row| row << "\n" }.join(' ')
+    @board_view = @board_array.clone.each { |row| row << "\n" }.join(' ')
   end
 end
 
@@ -101,18 +91,23 @@ def assert
   raise "Assertion failed" unless yield
 end
 
-sudoku = Sudoku.new("4-5269781682571493197834562826195347374682915951743628519326874248957136763418259")
-puts sudoku
+sudoku = Sudoku.new("---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---")
 sudoku.solve
-# puts sudoku
+
 
 # assert { sudoku.get_row_values == [4,'-',5,2,6,9,7,8,1] }
 # assert { sudoku.get_col_values == [4,6,1,8,3,9,5,2,7] }
 # assert { sudoku.empty_cells? == true }
-# # assert { sudoku.test_print_box == [4,'-',5,6,8,2,1,9,7] }  #@(row 0,0)
-# assert { sudoku.test_print_box == [4,'-',5,6,8,2,1,9,7] }  #@(row 0,1)
-# assert { sudoku.test_print_box == [8,2,6,3,8,4,9,5,1] }  #@(row 4,0)  fails
-# assert { sudoku.test_print_box == [2,6,9,5,7,1,8,3,4] }  #@(row 0,4)
+
+
+
+
+
+
+
+puts sudoku
+sudoku.solve
+
 # p sudoku.get_box_values
 
 puts "Great Success!"
