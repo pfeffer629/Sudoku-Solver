61ab7b8ab108121bf8d915061c2aa05fbe6d7153
julian.gallegos@gmail.com <julian.gallegos@gmail.com>
2014-10-11 13:40:46 -0700

    added get_row_nums function

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -28,30 +28,56 @@ class Sudoku
 
   def solve
     possibilities = (1..9).to_a
-
-
-    #
   end
 
-  def get_row
+  def get_rows
     @sudoku.each do |row|
-      if row != "-"
-        values << row
-      end
-    end
-    values
+     row
+    end 
+   end
+
+  def get_row_nums
+  	@row_nums_used = []
+  	row_zero = @sudoku[0]
+  	row_zero.map! do  
+  		|n| n[(/\d/)].to_i
+  	end
+  	#converts nil values to 0 and then remove 0 from array
+  	row_zero.reject! {|nums| nums == 0}
+  	p row_zero
   end
 
-  def get_col
+  def get_nums_from_column
+  end
+
+  def get_nums_from_box
+  end
+
+  def check_all_nums
+  	#checks numbers from nums_from_row, nums_from_col, 
+  		#and nums_from box
+  	#compares all those numbers from possible nums 1 to 9
+  	#and returns list of numbers not yet used
+  end
+
+  def remove_used_nums
+  	#compares list of all numbers used to possible numbers list
+  	#as there are duplicates, remove from possible number list
+  	#when there is only 1 unused number, put that number into
+  	# the empty cell area
+  end
+
+  def get_cols
     @sudoku.transpose.each do |col|
-      p col
+      col
     end
   end
+
 
   def get_box
     box_row = @sudoku[0..2]
     p box_row
-    end
+  end
 
 
   def find_empty_cells
@@ -66,11 +92,7 @@ class Sudoku
     p coords
   end
 
-  def check_row
-    #checking for numbers that haven't been used
 
-
-  end
 
   # def check_column
   # end
@@ -83,6 +105,7 @@ class Sudoku
   def to_s
     puts "#{@sudoku}"
   end
+
 end
 
 board = '---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---'
@@ -90,9 +113,10 @@ board = '---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--3
 game = Sudoku.new(board)
 # p game
 # p game.board
-p game.get_row
-# puts "-"*20
-# p game.get_col
+# p "this are the rows #{game.get_rows}"
+puts "-"*20
+p game.get_row_nums
+# p game.get_cols
 # puts "-"*20
 # p game.find_empty_cells
 # puts "-"*20
