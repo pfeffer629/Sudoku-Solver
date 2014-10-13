4130933aeb44ca1653d4fb0c37c62b239454f756
Kiope Gyzen <kiopelani@gmail.com>
2014-10-12 19:36:23 -0700

    Sudoku 1 Solution

--- source/runner.rb
+++ source/runner.rb
@@ -1,5 +1,59 @@
 require_relative 'sudoku'
 
+board_1 = "---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---"
+board_2 = "--5-3--819-285--6-6----4-5---74-283-34976---5--83--49-15--87--2-9----6---26-495-3"
+board_3 = "29-5----77-----4----4738-129-2--3-648---5--7-5---672--3-9--4--5----8-7---87--51-9"
+board_4 = "-8--2-----4-5--32--2-3-9-466---9---4---64-5-1134-5-7--36---4--24-723-6-----7--45-"
+board_5 = "6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--"
+board_6 = "---6891--8------2915------84-3----5-2----5----9-24-8-1-847--91-5------6--6-41----"
+
+game_1 = Sudoku.new(board_1)
+game_2 = Sudoku.new(board_2)
+game_3 = Sudoku.new(board_3)
+game_4 = Sudoku.new(board_4)
+game_5 = Sudoku.new(board_5)
+# game_6 = Sudoku.new(board_6)
+
+puts "GAME 1"
+puts "Unsolved"
+game_1.to_s
+game_1.solve
+puts "Solved"
+game_1.to_s
+
+puts "GAME 2"
+puts "Unsolved"
+game_2.to_s
+game_2.solve
+puts "Solved"
+game_2.to_s
+
+puts "GAME 3"
+puts "Unsolved"
+game_3.to_s
+game_3.solve
+puts "Solved"
+game_3.to_s
+
+puts "GAME 4"
+puts "Unsolved"
+game_4.to_s
+game_4.solve
+puts "Solved"
+game_4.to_s
+
+puts "GAME 5"
+puts "Unsolved"
+game_5.to_s
+game_5.solve
+puts "Solved"
+game_5.to_s
+
+# puts "GAME 6"
+# game_6.to_s
+# game_6.solve
+# game_6.to_s
+
 # The sudoku puzzles that your program will solve can be found
 # in the sudoku_puzzles.txt file.
 #
@@ -13,8 +67,9 @@ require_relative 'sudoku'
 # so we call String#chomp to remove them.
 
 
-board_string = File.readlines('sudoku_puzzles.txt').first.chomp
+# board_string = File.readlines('sudoku_puzzles.txt').first.chomp
 
-game = Sudoku.new(board_string)
-game.solve
-puts game
+# game = Sudoku.new(board_string)
+#game.to_s
+# game.solve
+# game.to_s
--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,16 +1,167 @@
 class Sudoku
   def initialize(board_string)
+    @board_string = board_string
+    @board = @board_string.split('').each_slice(9).to_a
   end
 
   def solve
+    @solved = false
+    while @solved == false
+      fill_cells_if_possible
+      solved?
+    end
+    @board
   end
 
-  def board
-  end
-
-  # Returns a nicely formatted string representing the current state of the board
   def to_s
+    p "-" * 25
+      @board.each_with_index do |row, row_i|
+        row_string = "| "
+        p "-" * 25 if row_i == 3 || row_i == 6
+        row.each_with_index do |cell, col_i|
+          if col_i == 3 || col_i == 6
+            row_string << "| " + cell + " "
+          elsif col_i == 8
+            row_string << cell
+          else
+            row_string << cell + " "
+          end
+        end
+        row_string << " |"
+        p row_string
+      end
+    p "-" * 25
   end
+
+  private
+  def solved?
+    if @board.flatten.include?("-")
+      @solved = false
+    else
+      @solved = true
+    end
+  end
+
+  def cell_empty?(digit)
+    true if digit == "-"
+  end
+
+  def fill_cells_if_possible
+    @board.each_with_index do |row, row_i|
+      row.each_with_index do |cell_value, col_i|
+        if cell_empty?(cell_value)
+          fill_cell([row_i,col_i]) if find_possible_nums([row_i,col_i]).length == 1
+        end
+      end
+    end
+  end
+
+  def find_possible_nums(cell_coord)
+    @possible_nums = ("1".."9").to_a
+    @possible_nums -= check_row(cell_coord)
+    @possible_nums -= check_col(cell_coord)
+    @possible_nums -= check_box(cell_coord)
+  end
+
+  def check_row(cell_coord)
+    row_index = cell_coord[0]
+    @board[row_index].reject {|num| num == "-" }
+  end
+
+  def check_col(cell_coord)
+    columns_array = @board.transpose
+    col_index = cell_coord[1]
+    columns_array[col_index].reject {|num| num == "-" }
+  end
+
+  def check_box(cell_coord)
+    make_boxes_hash
+    box_key = find_box_key(cell_coord)
+    @boxes[box_key].reject {|num| num == "-"}
+  end
+
+  def make_boxes_hash
+    @boxes = {
+      "Box 1" => [],
+      "Box 2" => [],
+      "Box 3" => [],
+      "Box 4" => [],
+      "Box 5" => [],
+      "Box 6" => [],
+      "Box 7" => [],
+      "Box 8" => [],
+      "Box 9" => []
+    }
+    @board.each_with_index do |row,row_i|
+      row.each_with_index do |item,col_i|
+        if (0..2).include?(row_i)
+            if (0..2).include?(col_i)
+              @boxes["Box 1"] << item
+            elsif (3..5).include?(col_i)
+              @boxes["Box 2"] << item
+            elsif(6..8).include?(col_i)
+              @boxes["Box 3"] << item
+            end
+        elsif (3..5).include?(row_i)
+            if (0..2).include?(col_i)
+              @boxes["Box 4"] << item
+            elsif (3..5).include?(col_i)
+              @boxes["Box 5"] << item
+            elsif (6..8).include?(col_i)
+              @boxes["Box 6"] << item
+            end
+        elsif (6..8).include?(row_i)
+            if (0..2).include?(col_i)
+              @boxes["Box 7"] << item
+            elsif (3..5).include?(col_i)
+              @boxes["Box 8"] << item
+            elsif (6..8).include?(col_i)
+              @boxes["Box 9"] << item
+            end
+        end
+      end
+    end
+    @boxes
+  end
+
+  def find_box_key(cell_coord)
+    row_i = cell_coord[0]
+    col_i = cell_coord[1]
+    box_key = ""
+    if (0..2).include?(row_i)
+      if (0..2).include?(col_i)
+        box_key = "Box 1"
+      elsif (3..5).include?(col_i)
+        box_key = "Box 2"
+      else
+        box_key = "Box 3"
+      end
+    elsif (3..5).include?(row_i)
+      if (0..2).include?(col_i)
+        box_key = "Box 4"
+      elsif (3..5).include?(col_i)
+        box_key = "Box 5"
+      else
+        box_key = "Box 6"
+      end
+    else
+      if (0..2).include?(col_i)
+        box_key = "Box 7"
+      elsif (3..5).include?(col_i)
+        box_key = "Box 8"
+      else
+        box_key = "Box 9"
+      end
+    end
+    box_key
+  end
+
+  def fill_cell(cell_coord)
+    row = cell_coord[0]
+    col = cell_coord[1]
+    @board[row][col] = @possible_nums[0]
+  end
+
 end
 
 
