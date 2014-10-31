63cc59f3ea42e183325e2207effef6b4512c6349
& Amir Fayek <pair+jarredhare.afayek1@devbootcamp.com>
2014-10-31 16:50:29 -0700

    Add final solution to sudoku

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -3,22 +3,33 @@ class Sudoku
   POSSIBLE_NUMBERS = ('1'..'9').to_a
   def initialize(board_string)
     @board = make_board(board_string)
-    # p @board
-    # @columns = make_columns(@board)
   end
 
-  def find_and_replace_empties
+  def solve
+    until finished?
+      find_and_replace_empty
+    end
+  end
+
+  def find_and_replace_empty
     @board.each_with_index do |row, row_index|
       row.each_with_index do |cell, column_index|
-        if cell == '-'
-          viable_numbers = find_possible_numbers(row_index, column_index)
-          if viable_numbers.length == 1
-            insert_number(viable_numbers, row_index, column_index)
-          end
+        if cell == '-' && one_solution?(row_index, column_index)
+          insert_number(find_possible_numbers(row_index, column_index), row_index, column_index)
         end
       end
     end
   end
+
+  def find_possible_numbers(row, column)
+    POSSIBLE_NUMBERS - get_row(row) - get_column(column) - get_box(row, column)
+  end
+
+  def one_solution?(row_index, column_index)
+    find_possible_numbers(row_index, column_index).length == 1
+  end
+
+
 
   def insert_number(number, row, column)
     @board[row][column] = number.join
@@ -45,86 +56,32 @@ class Sudoku
     @board.map { |row| row[column] }
   end
 
-  def find_possible_numbers(row, column)
-    POSSIBLE_NUMBERS - get_row(row) - get_column(column) - get_box(row, column)
-  end
-
   def get_box(row, column)
-    result = []
-    if row.between?(0,2) && column.between?(0,2)
-      for i in 0..2
-        for j in 0..2
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(0,2) && column.between?(3,5)
-      for i in 0..2
-        for j in 3..5
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(0,2) && column.between?(6,8)
-      for i in 0..2
-        for j in 6..8
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(3,5) && column.between?(0,2)
-      for i in 3..5
-        for j in 0..2
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(3,5) && column.between?(3,5)
-      for i in 3..5
-        for j in 3..5
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(3,5) && column.between?(6,8)
-      for i in 3..5
-        for j in 6..8
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(6,8) && column.between?(0,2)
-      for i in 6..8
-        for j in 0..2
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(6,8) && column.between?(3,5)
-      for i in 6..8
-        for j in 3..5
-        result << @board[i][j]
-        end
-      end
-    elsif row.between?(6,8) && column.between?(6,8)
-      for i in 6..8
-        for j in 6..8
-        result << @board[i][j]
-        end
+    box_values = []
+    start_box_row_value = row - row % 3
+    start_box_column_value = column - column % 3
+    (0..2).to_a.each do |row_offset|
+      (0..2).to_a.each do |column_offset|
+        box_values << @board[row_offset + start_box_row_value][column_offset + start_box_column_value]
       end
     end
-    result
+    box_values
   end
-
-
   # Returns a nicely formatted string representing the current state of the board
   def print_board
     @board.each { |row| p row }
   end
+
+
 end
 
 
-game = Sudoku.new('--5-3--819-285--6-6----4-5---74-283-34976---5--83--49-15--87--2-9----6---26-495-3')
+game = Sudoku.new('6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--')
 
 # p game.get_row(1)
 # p game.get_column(3)
 # game.insert_number(game.find_possible_numbers(0,1), 0, 1)
 
 # p game.find_possible_numbers(2,2)
-until game.finished?
-  game.find_and_replace_empties
-end
+game.solve
 game.print_board
