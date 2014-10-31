8657a4f1af12f2ff8f8716128ce9adf50821f360
Christopher Hui & Joey Samaniego <pair+christopherhuii.jellysalmon@devbootcamp.com>
2014-10-31 16:08:09 -0700

    added cluster_array

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -3,6 +3,8 @@ class Sudoku
   def initialize(board_string)
     @board_string = board_string
     @column_array = []
+    @cluster_stage = []
+    @cluster_array = []
   end
   def create_rows #array of 9 arrays of 9 strings
     # @array_of_strings_rows = @board_string.scan(/.{1,9}/m)
@@ -15,12 +17,67 @@ class Sudoku
 
   def create_columns
     9.times do |i|
-      @column_array << create_column(i)
+      @column_array << single_column(i)
     end
       @column_array
   end
 
-  def
+  def create_clusters
+      3.times do |i|
+        @cluster_stage << @rows[i][0..2]
+      end
+      3.times do |i|
+        @cluster_stage << @rows[i][3..5]
+      end
+      3.times do |i|
+        @cluster_stage << @rows[i][6..9]
+      end
+
+    @cluster_array << @cluster_stage[0..2].reduce(:+)
+    @cluster_array << @cluster_stage[3..5].reduce(:+)
+    @cluster_array << @cluster_stage[6..8].reduce(:+)
+
+   # @cluster_array[0].concat(@cluster_array[1]).concat(@cluster_array[2])
+   # @cluster_array[3].concat(@cluster_array[4]).concat(@cluster_array[5])
+   # @cluster_array[6].concat(@cluster_array[7]).concat(@cluster_array[8])
+
+    3.times do |i|
+        @cluster_stage << @rows[i+3][0..2]
+      end
+      3.times do |i|
+        @cluster_stage << @rows[i+3][3..5]
+      end
+      3.times do |i|
+        @cluster_stage << @rows[i+3][6..9]
+      end
+
+    @cluster_array << @cluster_stage[9..11].reduce(:+)
+    @cluster_array << @cluster_stage[12..14].reduce(:+)
+    @cluster_array << @cluster_stage[15..17].reduce(:+)
+
+
+    3.times do |i|
+        @cluster_stage << @rows[i+6][0..2]
+      end
+      3.times do |i|
+        @cluster_stage << @rows[i+6][3..5]
+      end
+      3.times do |i|
+        @cluster_stage << @rows[i+6][6..9]
+      end
+
+    @cluster_array << @cluster_stage[18..20].reduce(:+)
+    @cluster_array << @cluster_stage[21..23].reduce(:+)
+    @cluster_array << @cluster_stage[24..26].reduce(:+)
+
+  # p @cluster_stage
+
+
+
+   p @cluster_array
+
+
+  end
 
   def split_into_cells #
      @board_string.split('').each_slice(1).to_a
@@ -43,16 +100,18 @@ board1 = Sudoku.new('---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93-
 puts "Test create_rows"
 p board1.create_rows
 
-puts "Test create_columns"
+# puts "Test create_columns"
 
 # p board1.create_column
 
-p board1.collect_columns
+# # board1.create_columns
 # puts "Test split_into_cells"
 # p board1.split_into_cells
 
 #access the first row
 # p board1.create_rows[0]
 
-# access the first three indeces of the first row
-# p board1.create_rows[0](0..2) # => '---'
+# # access the first three indeces of the first row
+# # p board1.create_rows[0](0..2) # => '---'
+puts "CLUSTERS OF THREE CELLS"
+ board1.create_clusters
