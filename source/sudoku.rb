ca991da01c0b754b7b7e299d1211321779352b34
& Amir Fayek <pair+jarredhare.afayek1@devbootcamp.com>
2014-10-31 15:27:41 -0700

    Add initial solution

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,23 +1,27 @@
 class Sudoku
   attr_reader :board
+  POSSIBLE_NUMBERS = ('1'..'9').to_a
   def initialize(board_string)
     @board = make_board(board_string)
-    @possible_numbers = ('1'..'9').to_a
     # p @board
     # @columns = make_columns(@board)
   end
 
-  def find_empty_cell
+  def find_and_replace_empties
     @board.each_with_index do |row, row_index|
       row.each_with_index do |cell, column_index|
         if cell == '-'
-          possible_numbers = find_possible_numbers(row_index, column_index)
-          if possible_numbers.length == 1
-            insert_number(possible_number.first, row_index, column_index)
+          viable_numbers = find_possible_numbers(row_index, column_index)
+          if viable_numbers.length == 1
+            insert_number(viable_numbers, row_index, column_index)
           end
         end
       end
     end
+  end
+
+  def insert_number(number, row, column)
+    @board[row][column] = number.join
   end
 
   def finished?
@@ -42,11 +46,69 @@ class Sudoku
   end
 
   def find_possible_numbers(row, column)
-    @possible_numbers - get_row(0) - get_column(0)
+    POSSIBLE_NUMBERS - get_row(row) - get_column(column) - get_box(row, column)
   end
 
-  def board
+  def get_box(row, column)
+    result = []
+    if row.between?(0,2) && column.between?(0,2)
+      for i in 0..2
+        for j in 0..2
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(0,2) && column.between?(3,5)
+      for i in 0..2
+        for j in 3..5
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(0,2) && column.between?(6,8)
+      for i in 0..2
+        for j in 6..8
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(3,5) && column.between?(0,2)
+      for i in 3..5
+        for j in 0..2
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(3,5) && column.between?(3,5)
+      for i in 3..5
+        for j in 3..5
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(3,5) && column.between?(6,8)
+      for i in 3..5
+        for j in 6..8
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(6,8) && column.between?(0,2)
+      for i in 6..8
+        for j in 0..2
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(6,8) && column.between?(3,5)
+      for i in 6..8
+        for j in 3..5
+        result << @board[i][j]
+        end
+      end
+    elsif row.between?(6,8) && column.between?(6,8)
+      for i in 6..8
+        for j in 6..8
+        result << @board[i][j]
+        end
+      end
+    end
+    result
   end
+
 
   # Returns a nicely formatted string representing the current state of the board
   def print_board
@@ -55,8 +117,14 @@ class Sudoku
 end
 
 
-game = Sudoku.new('---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---')
-game.print_board
+game = Sudoku.new('--5-3--819-285--6-6----4-5---74-283-34976---5--83--49-15--87--2-9----6---26-495-3')
+
 # p game.get_row(1)
 # p game.get_column(3)
-p game.solve
+# game.insert_number(game.find_possible_numbers(0,1), 0, 1)
+
+# p game.find_possible_numbers(2,2)
+until game.finished?
+  game.find_and_replace_empties
+end
+game.print_board
