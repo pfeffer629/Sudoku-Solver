0c425a0454e7a790c87424978856a7b9556cf342
Sharmila Jesupaul & Tina Tien <pair+sharmilajesupaul.ttien28@devbootcamp.com>
2014-10-31 19:26:57 -0700

    added sudoku logic challenge

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -6,11 +6,26 @@ class Sudoku
     fill_board
   end
 
+  def solve
+    possible = []
+    unless board_filled?
+      @board.each_with_index do |row, row_index|
+        row.each_with_index do |cell, col_index|
+          if find_empty_cell == [row_index, col_index]
+            possible = VALUES - (check_row(row_index) + check_col(col_index) + check_quadrant(row_index,col_index) )
+            if possible.length == 1
+              @board[row_index][col_index] = possible[0]
+            end
+          end
+        end
+      end
+    end
+    @board
+  end
+
   def fill_board
     @board = @board_string.split("").each_slice(9).to_a
     @board.map { |row| row.map! { |cell| cell == "-" ? nil : cell.to_i }}
-
-
     p @board
   end
 
@@ -36,42 +51,20 @@ class Sudoku
     quandrants[(row / 3) * 3 + (col / 3)]
   end
 
-
-  def solve
-    board_filled?
-    possible = []
-    # replace_empty_cells
-    possible = VALUES - check_row(0)
-    possible
-  end
-
   def board_filled?
     @board.each { |row| row.each { |cell| return false if cell.nil?  } }
     true
   end
 
   def find_empty_cell
-    # p @board
-    # @board.each do |row|
-    #   # puts "inside row"
-    #   row.map! do |cell|
-    #     # puts "inside cell"
-    #     if cell == "- " || cell == "-"
-    #       ((1..9).to_a)
-    #     elsif cell.is_a? String
-    #         cell.ljust(1).to_i
-    #     else
-    #       cell
-    #     end
-    #   end
-    # end
-    # @board
-
+    @board.each_with_index do |row, row_i|
+      row.each_with_index do |cell, col_i|
+        return [row_i, col_i] if cell.nil?
+      end
+    end
   end
-  def check_row row
-    # eliminate = []
-    # array_indices = []
 
+  def check_row row
     ints = []
     get_row(row).each do |cell|
       if cell.is_a? Integer
@@ -79,26 +72,26 @@ class Sudoku
       end
     end
     ints
-    # iterates through each row of the @board
-    # @board.each do |row|
-    #   # iterates through each cell in the row
-    #   row.each_with_index do |cell, index|
-    #     # if the cell is an array
-    #     if cell.is_a? Array
-    #       # push the index of the cell to the array_indices
-    #       array_indices << index
-    #       p array_indices
-    #       # else if cell is an integer
-    #     elsif cell.is_a? Integer
-    #       # loop through our array_indices and
-    #       # delete each integer value in the row[index from array_indices]
-    #       # row[array_index].delete(cell)
-    #       array_indices.each do |array_index|
-    #         p row[array_index]
-    #       end
-    #     end
-    #   end
-    # end
+  end
+
+  def check_col col
+    ints = []
+    get_col(col).each do |cell|
+      if cell.is_a? Integer
+        ints << cell
+      end
+    end
+    ints
+  end
+
+  def check_quadrant(col, row)
+    ints = []
+    get_quadrant(row, col).each do |cell|
+      if cell.is_a? Integer
+        ints << cell
+      end
+    end
+    ints
   end
 
   # def board
@@ -108,28 +101,38 @@ class Sudoku
   def to_s
   # formatted_board = @board.map do |row|
   #                      row.map! {|char| "-" if char.nil? }
-  #                     row.join
+  #                       row.join
   #                   end
   # puts formatted_board
   #   ""
-  # end
+  end
 end
 
 
 ### Driver Test Code #######
 board1 = Sudoku.new("---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---")
-# puts board1
-# board1.replace_empty_cells
-puts '-' * 20
+
+# p board1.solve
+# p board1.board_filled?
+# # p board1.get_row(0)
+# # p board1.get_col(0)
+# # p board1.check_row(0)
+# # p board1.check_col(1)
+# # p board1.check_quadrant(0,1)
+# # p board1.get_quadrant(0, 0)
+# p board1.find_empty_cell
+# p board1.solve
+puts '-' * 10
 board2 = Sudoku.new("4-5269781682571493197834562826195347374682915951743628519326874248957136763418259")
-p board2
-p board2.solve
+# p board2
+
 p board2.board_filled? == false
 p board2.get_row(0) == [4, nil, 5, 2, 6, 9, 7, 8, 1]
 p board2.get_col(0) == [4, 6, 1, 8, 3, 9, 5, 2, 7]
 p board2.check_row(0) == [4, 5, 2, 6, 9, 7, 8, 1]
-# p board2.fill_board
-# board2.replace_empty_cells
-# p board2.board
-p board2.get_quadrant(0, 0)
-puts board2
+p board2.check_col(1) == [8, 9, 2, 7, 5, 1, 4, 6]
+p board2.check_quadrant(0,1) == [4, 5, 6, 8, 2, 1, 9, 7]
+p board2.get_quadrant(0, 0) == [4, nil, 5, 6, 8, 2, 1, 9, 7]
+p board2.find_empty_cell == [0,1]
+p board2.solve
+# puts board2
