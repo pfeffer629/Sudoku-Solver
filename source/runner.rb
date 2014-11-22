0102f81ba38405f30e7c7fcf12379684337fb742
gaillechua & jgerminario <pair+gaillechua.jgerminario@devbootcamp.com>
2014-11-21 19:19:40 -0800

    Almost thereeeeee

--- source/runner.rb
+++ source/runner.rb
@@ -17,11 +17,30 @@ require_relative 'sudoku'
 
 # game = Sudoku.new(board_string)
 
-board_string = '123456789234567891345-78912456789123567891234678912345789123456891234567912345678'
+board_string = '---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---'
+# board_string = '-234567-9234567291345678912456789123567891234678912345789123456891234567912345678'
+
 game = Sudoku.new(board_string)
 
-# p game.board == '-234567-9234567891345678912456789123567891234678912345789123456891234567912345678'
+"- - - 2 6 - 7 - 1"
+"6 8 - - 7 - - 9 -"
+"1 9 - - - 4 5 - -"
+"8 2 - 1 - - - 4 -"
+"- - 4 6 - 2 9 - -"
+"- 5 - - - 3 - 2 8"
+"- - 9 3 - - - 7 4"
+"- 4 - - 5 - - 3 6"
+"7 - 3 - 1 8 - - -"
 
+"- 3 5 2 6 0 7 8 1"
+"6 8 2 5 7 1 - 9 3"
+"1 9 7 8 3 4 5 6 2"
+"8 2 6 1 0 - 3 4 -"
+"3 - 4 6 - 2 9 - -"
+"- 5 - - - 3 - 2 8"
+"- - 9 3 - - - 7 4"
+"- 4 - - 5 - - 3 6"
+"7 - 3 - 1 8 - - -"
 
 # game.find_blank == [0, 0]
 # #  puts game
@@ -30,16 +49,22 @@ game = Sudoku.new(board_string)
 # # puts game
 # p game.number_check == "1"
 # p game.insert_number
+# puts game
+puts game
+
+game.solve
 puts game
 
 
-p game.find_blank
+# puts game
+# game.solve
+# puts game
 
-p game.check_box
 
 
 
-game.solve
+
+# game.solve
 
 
 
--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,4 +1,5 @@
 class Sudoku
+  attr_reader :board_arr, :check_arr
   def initialize(board_string)
     @board_string = board_string
     @board_arr = board_string.split("")
@@ -12,16 +13,8 @@ class Sudoku
 
   def solve
     until find_blank == false
-      find_next
+      find_blank
     end
-  end
-
-  def find_next
-    find_blank
-    check_row
-    check_col
-    number_check
-    insert_number
   end
 
   def find_blank
@@ -29,46 +22,66 @@ class Sudoku
     @board_arr.each_with_index do |row, row_ind|
       row.each_with_index do |value, col_ind|
         if value == "-"
-          @blank_pos = [row_ind, col_ind]
-          return @blank_pos
+          p @blank_pos = [row_ind, col_ind]
+          @check_arr = []
+          check_row
+          check_col
+          check_box
+          number_check
+          insert_number
+          puts self
         end
       end
     end
-    return false
   end
 
   def check_row
-    @check_arr = []
     @row_arr = @board_arr[@blank_pos[0]].select {|v| v =~ /[1-9]/}
     @check_arr = @check_arr.concat(@row_arr)
-    @check_arr.sort!
+    p @check_arr = @check_arr.uniq.sort
   end
 
   def check_col
     @trans_board = @board_arr.transpose
     @col_arr = @trans_board[@blank_pos[1]].select {|v| v =~ /[1-9]/}
     @check_arr = @check_arr.concat(@col_arr)
-    @check_arr.uniq!.sort!
+    p @check_arr = @check_arr.uniq.sort
   end
 
   def check_box
     @box_pos = []
-    @box_pos << @blank_pos[0]%3
-    @box_pos << @blank_pos[1]%3
-    @box_pos
+    @row_trans = @blank_pos[0]/3 #1
+    @col_trans = @blank_pos[1]/3 #0
+    @box_pos << @blank_pos[0]%3 #2
+    @box_pos << @blank_pos[1]%3 #0
+    @box_checks = []
+    @box_checks << @board_arr[(@box_pos[0]+1)%3+@row_trans*3][(@box_pos[1]+1)%3+@col_trans*3]
+    @box_checks << @board_arr[(@box_pos[0]+2)%3+@row_trans*3][(@box_pos[1]+1)%3+@col_trans*3]
+    @box_checks << @board_arr[(@box_pos[0]+1)%3+@row_trans*3][(@box_pos[1]+2)%3+@col_trans*3]
+    @box_checks << @board_arr[(@box_pos[0]+2)%3+@row_trans*3][(@box_pos[1]+2)%3+@col_trans*3]
+    @box_checks
+    @box_arr = @box_checks.select {|v| v =~ /[1-9]/}
+    @check_arr = @check_arr.concat(@box_arr)
+    p @check_arr = @check_arr.uniq.sort
   end
 
   def number_check
-    @missing_number = ""
-    9.times do |n|
-      if not @check_arr.include?(@poss_arr[n-1])
-        return @missing_number = n.to_s
+    if @check_arr.length == 8
+      @missing_number = "X"
+      9.times do |n|
+        if not @check_arr.include?(@poss_arr[n])
+          return @missing_number = n.to_s
+        end
       end
+    else
+      false
     end
   end
 
   def insert_number
-    @board_arr[@blank_pos[0]][@blank_pos[1]] = @missing_number
+    if number_check
+      @board_arr[@blank_pos[0]][@blank_pos[1]] = @missing_number
+    end
   end
 
   def board
