f2f538835e553ff6cfe7fd6a6261de99c03b0582
WhaleMonster <xuboxes@gmail.com>
2014-11-22 00:00:02 -0800

    Benchmark tests

--- source/runner.rb
+++ source/runner.rb
@@ -13,13 +13,16 @@ require 'benchmark'
 # Remember, the file has newline characters at the end of each line,
 # so we call String#chomp to remove them.
 
-sum = 0
-100.times do
-start = Time.now
-Sudoku.new('---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---').solve
-finish = Time.now
-sum += finish - start
-end
+# sum = 0
+# 100.times do
+# start = Time.now
+# Sudoku.new('---26-7-168--7--9-19---45--82-1---4---46-29---5---3-28--93---74-4--5--367-3-18---').solve
+# finish = Time.now
+# sum += finish - start
+# end
 
-p sum/100
+# p sum/100
+
+
+# averages about 0.0023 seconds, twice as slow as Peter Norvig
 
