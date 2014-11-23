3cdcdb27bf987256b083d314c3ce6170b576a071
Forrest Venable <forrest.venable@gmail.com>
2014-11-22 16:49:50 -0800

    Code runs, doesn't do what it is supposed to

--- source/runner.rb
+++ source/runner.rb
@@ -17,4 +17,3 @@ board_string = File.readlines('sudoku_puzzles.txt').first.chomp
 
 game = Sudoku.new(board_string)
 game.solve!
-puts game
--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,29 +1,30 @@
 class Sudoku
   def initialize(board_string)
-    @board = board_string.split(//).each_slice(9)
-    @test_array = ['1','2','3','4','5','6','7','8','9']
+    @board = []
+    board_string.split(//).each_slice(9){|element| @board << element}
+    @test_array = ["1","2","3","4","5","6","7","8","9"]
     @boxes = [
-      [[0,0],[0,1],[0,3],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]], #box_1
+      [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]], #box_1
       [[0,3],[0,4],[0,5],[1,3],[1,4],[1,5],[2,3],[2,4],[2,5]], #box_2
       [[0,6],[0,7],[0,8],[1,6],[1,7],[1,8],[2,6],[2,7],[2,8]], #box_3
-      [[3,0],[3,1],[3,3],[4,0],[4,1],[4,2],[5,0],[5,1],[5,2]], #box_4
+      [[3,0],[3,1],[3,2],[4,0],[4,1],[4,2],[5,0],[5,1],[5,2]], #box_4
       [[3,3],[3,4],[3,5],[4,3],[4,4],[4,5],[5,3],[5,4],[5,5]], #box_5
       [[3,6],[3,7],[3,8],[4,6],[4,7],[4,8],[5,6],[5,7],[5,8]], #box_6
-      [[6,0],[6,1],[6,3],[7,0],[7,1],[7,2],[8,0],[8,1],[8,2]], #box_7
+      [[6,0],[6,1],[6,2],[7,0],[7,1],[7,2],[8,0],[8,1],[8,2]], #box_7
       [[6,3],[6,4],[6,5],[7,3],[7,4],[7,5],[8,3],[8,4],[8,5]], #box_8
       [[6,6],[6,7],[6,8],[7,6],[7,7],[7,8],[8,6],[8,7],[8,8]] #box_9
     ]
+    p @board.flatten
   end
 
 # find values in row/column/box respectively
-
-  def row(index, board)
-    board[index]
+  def row(index)
+    @board[index]
   end
 
-  def column(index, board)
+  def column(index)
     column = []
-    board.each do |row|
+    @board.each do |row|
       column << row[index]
     end
     column
@@ -33,7 +34,9 @@ class Sudoku
     box_values = []
     y = 0
     x = 0
+    box
     @boxes[box].each do |coords|
+      coords
       y = coords[0]
       x = coords[1]
       box_values << @board[y][x]
@@ -48,91 +51,98 @@ class Sudoku
         return index if coords == [y, x]
       end
     end
-    false
   end
 
 #checks for duplicates in row/column/box
-  def valid_board?(board)
-    9.times do |index|
-      return false unless valid_group?(column(index, board))
-      return false unless valid_group?(row(index, board))
-      return false unless valid_group?(box(index, board))
-    end
-    true
-  end
+#tests don't work
+ #  def valid_board?(board)
+ #    9.times do |index|
+ #      return false if !valid_group?(column(index, board))
+ #      return false if !valid_group?(row(index, board))
+ #      return false if !valid_group?(box(index, board))
+ #    end
+ #    true
+ #  end
 
-#do these work?
-  def valid_group(array)
-	array.each do |cell|
-	  next if cell == '-'
-      return false if !(@test_array.one?{|sudoku_element| sudoku_element == cell} || @test_array.none?{|sudoku_element| sudoku_element == cell})
-    end
-    true
-  end
 
-  def valid_row?(row)
-    row.each do |cell|
-      return false if !@test_array.one?{|sudoku_element| sudoku_element == cell}
-    end
-    true
-  end
-  # 8 numbers - returns true
+ #  def valid_group?(array)
+	# array.each do |cell|
+	#   next if cell == '-'
+ #    return false if !@test_array.one?{|sudoku_element| sudoku_element == cell}
+ #    end
+ #    true
+ #  end
+#these three unused
+  # def valid_row?(row)
+  #   row.each do |cell|
+  #     return false if !@test_array.one?{|sudoku_element| sudoku_element == cell}
+  #   end
+  #   true
+  # end
+  # # 8 numbers - returns true
 
-  def valid_column?(column)
-    column.each do |cell|
-      next if cell == '-'
-      return false if !@test_array.one?{|sudoku_element| sudoku_element == cell}
-    end
-    true
-  end
+  # def valid_column?(column)
+  #   column.each do |cell|
+  #     next if cell == '-'
+  #     return false if !@test_array.one?{|sudoku_element| sudoku_element == cell}
+  #   end
+  #   true
+  # end
 
-  def valid_box?(box)
-    row.each do |cell|
-      return false if !@test_array.one?{|sudoku_element| sudoku_element == cell}
-    end
-    true
-  end
+  # def valid_box?(box)
+  #   row.each do |cell|
+  #     return false if !@test_array.one?{|sudoku_element| sudoku_element == cell}
+  #   end
+  #   true
+  # end
 
   def possible(y_coord, x_coord)
-    possible = []
-    @test_array.each do |sudoku_element|
-      possible << sudoku_element if test_number?(sudoku_element, y_coord, x_coord)
+    # possibilities = []
+    # @test_array.each do |sudoku_element|
+    #   possiblities << sudoku_element if test_number?(sudoku_element, y_coord, x_coord)
+    # end
+    # possibilities
+    @test_array.reject do |sudoku_element|
+      row(y_coord).include?(sudoku_element) ||
+      column(x_coord).include?(sudoku_element) ||
+      box(in_box(y_coord, x_coord)).include?(sudoku_element)
     end
-    possible
   end
 
-  def test_number?(string, y_coord, x_coord)
-    test_board = @board.clone
-    test_board[y_coord][x_coord] = string
-    valid_board?(test_board)
-  end
+  # def test_number?(string, y_coord, x_coord)
+  #   test_board = @board.clone
+  #   test_board[y_coord][x_coord] = string
+  #   valid_board?(test_board)
+  # end
 
   def solve!
-    while !solved? do
+    changed = true
+    while !solved? && changed == true do
+      changed = false
       @board.each_with_index do |row, y|
         row.each_with_index do |element, x|
-          next unless element == '-'
-
+          next unless element == "-"
+          if possible(y, x).one?
+            p @board[y][x] = possible(y, x)[0]
+            changed = true
+          end
         end
       end
     end
+    p solved?
+    p @board
   end
 
   def solved?
-    !@board.flatten.include?('-')
+    !@board.flatten.include?("-")
   end
 
   def board
   end
 
-  # Returns a nicely formatted string representing the current state of the board
+#  Returns a nicely formatted string representing the current state of the board
   def to_s
-    @board.each do |row|
-      row.each do |element|
-        print element
-      end
-      print "\n"
-    end
+    @board.to_s
   end
 end
 
