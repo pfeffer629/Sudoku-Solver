20dfa2c254ad1ed424635b5c07a4caafb7033157
jgerminario <jgerminario@gmail.com>
2014-11-24 22:06:14 -0800

    added notes

---
+++ source/runner_notes.rb
@@ -0,0 +1,72 @@
+require_relative 'sudoku'
+require 'benchmark'
+
+# The sudoku puzzles that your program will solve can be found
+# in the sudoku_puzzles.txt file.
+#
+# Currently, Line 16 defines the variable board_string to equal
+# the first puzzle (i.e., the first line in the .txt file).
+# After your program can solve this first puzzle, edit
+# the code below, so that the program tries to solve
+# all of the puzzles.
+#
+# Remember, the file has newline characters at the end of each line,
+# so we call String#chomp to remove them.
+
+
+# board_string = File.readlines('sudoku_puzzles.txt').first.chomp
+
+# game = Sudoku.new(board_string)
+
+board_string = '6-873----2-----46-----6482--8---57-19--618--4-31----8-86-2---39-5----1--1--4562--'
+# board_string = '-234567-9234567291345678912456789123567891234678912345789123456891234567912345678'
+game = Sudoku.new(board_string)
+
+# empty_cell = game.find_blank - getting the return value from a method and testing off of this is a great way to ensure you have access to information within the method for the next one
+
+#row_vals = game.row_vals_for(empty_cell[0])
+# p row_vals == ["4", "-"...]
+# box_vals = game.box_vals_for(empty_cell) - make all of these testable, organize your test cases so they are always available. - sending empty cell into these
+# If you see 'empty_cell' getting passed around a lot, probably an attribute
+
+
+
+p game.board_arr.flatten.index("8")
+
+p game.find_blank == [0,1]
+
+# p game.corr_values(empty_cell) - would then give back row vals, col vals, etc.
+
+
+# p game.board_arr.flatten
+
+# game.find_blank == [0, 0]
+# #  puts game
+# p game.check_row #== %w(2 3 4 5 6 7 9)
+# p game.check_col
+# # puts game
+# p game.number_check == "1"
+# p game.insert_number
+# puts game
+# puts game
+
+# game.solve
+
+
+# puts game
+# game.solve
+# puts game
+
+
+# game.solve
+
+# puts game
+# game.solve
+
+
+puts Benchmark.measure {}
+
+
+# puts game
+# # game.solve
+# # puts game
---
+++ source/sudoku_notes.rb
@@ -0,0 +1,113 @@
+class Sudoku
+  attr_reader :board_arr, :check_arr
+  def initialize(board_string)
+    ## Can make a string instead, perhaps? # Can also give it 
+    @board_arr = board_string.split("") #Refactor to get the split arr on one line
+    @board_string = board_string
+    @poss_arr = %w(1 2 3 4 5 6 7 8 9)
+    split_arr_to_9_rows
+  end
+
+  def split_arr_to_9_rows
+    @board_arr = @board_arr.each_slice(9).to_a
+  end
+
+  def solve
+    @no_hits = 0
+    until is_incomplete? == false
+      find_blank
+    end
+  end
+
+  def is_incomplete? # Is complete
+    return false if not @board_arr.flatten.include?("-")
+  end
+
+  def find_blank # Find next blank - a little more descriptive - next blank coords, e.g.
+    # Is this the right place for this whole loop? Maybe not
+    @board_arr.each_with_index do |row, row_ind|
+      row.each_with_index do |value, col_ind|
+        if value == "-"
+          @blank_pos = [row_ind, col_ind]
+          @check_arr = []
+          check_row
+          check_col
+          check_box
+          number_check
+          insert_number
+          escape_if_complex
+        end
+      end
+    end
+  end
+
+  def find_blank
+    [@board_string.index("-")/9, @board_string.index("-")%9] #can do this with an index in the same way you could a string.
+    # @board_arr.flatten.index("-").divmod(9) #Gives the divide and the modolo of that number. Allows you to basically take a structure of the same height and width. Index gives the position of that item you are looking for. 
+  end
+
+  def check_row #Separate methods for defining and checking row?
+    @check_arr += @board_arr[@blank_pos[0]].select {|v| v =~ /[1-9]/}
+  end
+
+  def check_col
+    @check_arr += @board_arr.transpose[@blank_pos[1]].select {|v| v =~ /[1-9]/}
+  end
+
+  def identify_the_4_remaining_in_box
+    @box_pos = [@blank_pos[0]%3, @blank_pos[1]%3]
+    @row_return = @blank_pos[0]/3 
+    @col_return = @blank_pos[1]/3 
+    @box_checks = []
+    2.times do |r|
+      2.times do |c|
+       @box_checks << @board_arr[(@box_pos[0]+r+1)%3+@row_return*3][(@box_pos[1]+c+1)%3+@col_return*3]
+      end
+    end
+  end
+
+  def check_box
+    identify_the_4_remaining_in_box
+    @check_arr += @box_checks.select {|v| v =~ /[1-9]/}
+  end
+
+  def number_check
+    @check_arr = @check_arr.uniq.sort
+    if @check_arr.length == 8
+      @no_hits = 0
+      @missing_number = "X"
+      9.times do |n|
+        unless @check_arr.include?(@poss_arr[n])
+          return @missing_number = @poss_arr[n].to_s
+        end
+      end
+    else
+      @no_hits += 1
+      false
+    end
+  end
+
+  def insert_number
+    if number_check
+      @board_arr[@blank_pos[0]][@blank_pos[1]] = @missing_number
+    end
+  end
+
+  def escape_if_complex
+    if @no_hits >= 81
+      abort("This problem is too complex for this Sudoku solver")
+    end
+  end
+
+  def board
+    @board_arr.join("")
+  end
+
+  def to_s
+    9.times do |r|
+      p @board_arr[r].join(" ")
+    end
+  end
+end
+
+
